6c4f77310fd27d1b19a4e960c4e00fd4d4d4ef1f
psnapyeditor <info.psnapy@gmail.com>
2024-07-17 14:21:52 +0300

    first commit

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -6,7 +6,7 @@ import Card from "../card/Card";
 
 const getData = async (page, cat) => {
   const res = await fetch(
-    `https://huberman-azure.vercel.app/api/posts?page=${page}&cat=${cat || ""}`,
+    `https://huberman-azure.vercel.app/api/postsimg?page=${page}&cat=${cat || ""}`,
     {
       cache: "no-store",
     }

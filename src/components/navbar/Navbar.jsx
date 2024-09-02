8e524f1302c4c9d318d84ac346f3934bd33ac3e5
psnapyeditor <info.psnapy@gmail.com>
2024-09-03 00:51:05 +0300

    main first commet

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,26 +6,73 @@ import Link from "next/link";
 import AuthLinks from "../authLinks/AuthLinks";
 import ThemeToggle from "../themeToggle/ThemeToggle";
 import { ThemeContext } from "@/context/ThemeContext";
-
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon as faMoonRegular, } from "@fortawesome/free-regular-svg-icons";
 const Navbar = () => {
-  const { theme } = useContext(ThemeContext);
+  const { toggle, theme } = useContext(ThemeContext);
 
-  const containerStyle = theme === "dark" ? { backgroundColor: "#020203" } : { backgroundColor: "#fcfeff" };
+  const containerStyle = theme === "dark" 
+  ? { backgroundColor: "rgba(1, 1, 2, 0.7)" } 
+  : { backgroundColor: "rgba(252, 254, 255, 0.7)" };
 
   return (
     <div className={styles.container} style={containerStyle}>
       <div className={styles.wrapper}>
         <Link href="/">
-          <div className={styles.logo}>Airnesi</div>
+          <div className={styles.logo}>
+            <Image src="/logo1.png" alt="airnesy logo" layout="fill" className={styles.logoImage} />
+            <h1 className={styles.logoText}>Reddit</h1>
+
+          </div>
         </Link>
+        <Link href="/">
+          <div className={styles.logo}>
+            <h1 className={styles.logoText}></h1>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className={styles.logo}>
+            <h1 className={styles.logoText}></h1>
+          </div>
+        </Link>     <Link href="/">
+          <div className={styles.logo}>
+            <h1 className={styles.logoText}></h1>
+          </div>
+        </Link>     <Link href="/">
+          <div className={styles.logo}>
+            <h1 className={styles.logoText}></h1>
+          </div>
+        </Link>     <Link href="/">
+          <div className={styles.logo}>
+            <h1 className={styles.logoText}></h1>
+          </div>
+        </Link>
+        <div className={styles.searchContainer}>
+          <input 
+            type="text" 
+            placeholder="Search..." 
+            className={styles.searchBar} 
+          />
+        </div>
         <div className={styles.links}>
-          <ThemeToggle />
-          <Link href="/" className={styles.link}>Homepage</Link>
-          <Link href="https://a.co/d/7yHmqai" className={styles.link}>Books</Link>
+          <div 
+            className={styles.theme}
+            onClick={toggle}
+          >
+                   <FontAwesomeIcon icon={faMoonRegular}  size="sm"  className={styles.themeImage} />
+          </div>
+
+
+
+     
           <AuthLinks />
         </div>
+        
       </div>
+
     </div>
+    
   );
 };
 

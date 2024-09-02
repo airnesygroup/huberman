fe9096f9f1778f6c3e99dcdf4cbee3ecfbc21232
psnapyeditor <info.psnapy@gmail.com>
2024-09-03 00:41:03 +0300

    main first commet

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,36 +3,88 @@ import styles from "./card.module.css";
 import Link from "next/link";
 
 const Card = ({ key, item }) => {
-  return (
-    <div className={styles.container} key={key}>
-      <div className={styles.header}>
-      <div className={styles.userProfile}>
-            <Image src={item.user?.image} alt={item.user?.name} className={styles.profileImage} width={25} height={25} />
-            <div className={styles.userInfo}>
-              <p className={styles.username}>{item.user?.name}</p>
-              <p className={styles.userRole}>{item.user?.role}</p>
-              
-            </div>
-            <span className={styles.date}>{item.createdAt.substring(0, 10)}</span>
+  const truncatedDesc = item?.desc.substring(0, 400);
+  const truncatedDesc2 = item?.desc.substring(0, 150);
 
-          </div>
-        <span className={styles.category}>{item.catSlug}</span>
-      </div>
-      <Link href={`/posts/${item.slug}`}>
-        <h1 className={styles.title}>{item.title.substring(0, 60)}</h1>
-      </Link>
-      <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 160) }} />
-      <Link href={`/posts/${item.slug}`} className={styles.link}>
-        Read Mor
-      </Link>
-      {item.img && (
-        <div className={styles.imageContainer}>
-          <Image src={item.img} alt="" fill className={styles.image} />
+  const showMore = item?.desc.length > 300;
+
+  return (
+    <Link href={`/posts/${item.slug}`} passHref>
+      <div className={styles.container} key={key}>
+        <div className={styles.profileContainer}>
+          <Image
+            src={item.user?.image}
+            alt={item.user?.name}
+            className={styles.profileImage}
+            width={25}
+            height={25}
+          />
+          <div className={styles.verticalLine}></div>
         </div>
-      )}
-    </div>
+        <div className={styles.infoContainer}>
+          <div className={styles.header}>
+            <div className={styles.userProfile}>
+              <div className={styles.userInfo}>
+                <div className={styles.userProfile}>
+                  <Image
+                    src={item.user?.image}
+                    alt={item.user?.name}
+                    className={styles.profileImage2}
+                    width={25}
+                    height={25}
+                  />
+                  <div className={styles.userInfo}>
+                    <p className={styles.username}>{item.user?.name}</p>
+                    <p className={styles.userRole}>{item.user?.role}</p>
+                  </div>
+                  <span className={styles.date}>
+                    {item.createdAt.substring(0, 10)}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <span className={styles.category}>{item.catSlug}</span>
+          </div>
+          <h1 className={styles.title}>{item.title.substring(0, 150)}</h1>
+          <h1 className={styles.title2}>{item.title.substring(0, 80)}</h1>
+
+          <div className={styles.descContainer}>
+            <div
+              className={styles.desc}
+              dangerouslySetInnerHTML={{ __html: truncatedDesc }}
+            />
+            <div
+              className={styles.desc2}
+              dangerouslySetInnerHTML={{ __html: truncatedDesc2 }}
+            />
+        
+          </div>
+          {item.img && (
+            <div className={styles.imageContainer}>
+              <div
+                className={styles.imageBackground}
+                style={{
+                  backgroundImage: `url(${item.img})`,
+                }}
+              />
+              <Image
+                src={item.img}
+                alt={item.title}
+                layout="intrinsic"
+                className={styles.image}
+                 // Ensures the image never exceeds 500px in height
+               
+              />
+            </div>
+          )}
+    
+    
+        </div>
+      </div>
+
+      
+    </Link>
   );
 };
 
 export default Card;
-

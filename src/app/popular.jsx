3df122b27990e65d686d6bd4415a93402934e8f2
psnapyeditor <info.psnapy@gmail.com>
2024-09-03 22:27:05 +0300

    main first commet

---
+++ src/app/popular.jsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import styles from "./popular.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import SidebarCategoryList from "@/components/SidebarcategoryList/SidebarCategoryList";
+import Menu from "@/components/Menu/Menu";
+import CarouselList from "@/components/carouselList/CarouselList";
+import Controls from "@/components/Controls";
+import Trending from "@/components/Trending";
+import AddIcon from "@/components/Addicon";
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
+
+export default function popular({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.trendingWrapper}
+      >
+        <Trending />
+      </div>
+
+     <Navbar />
+      <AddIcon />
+
+      <div className={styles.menu}>
+        <SidebarCategoryList />
+      </div>
+      <div className={styles.mainContent}>
+        <CategoryList />
+
+        <CarouselList page={page} />
+
+        <div className={styles.content1}>
+          <CardList page={page} />
+
+          <Menu />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}

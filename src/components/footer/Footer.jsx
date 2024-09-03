4bce5c61afa1a66098421f7c4b50b26bd24c1879
psnapyeditor <info.psnapy@gmail.com>
2024-09-03 16:16:43 +0300

    main first commet

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,38 +7,38 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.info}>
-        <div className={styles.logo}>
-          <Image src="/profile.png" alt="lama blog" width={50} height={50} />
-          <h1 className={styles.logoText}>airnesy</h1>
-        </div>
+      <Link href="/">
+          <div className={styles.logo}>
+            <Image src="/logo1.png" alt="airnesy logo" layout="fill" className={styles.logoImage} />
+            <h1 className={styles.logoText}>Reddit</h1>
+          </div>
+        </Link>
         <p className={styles.desc}>
-      This blog is frequently ranked in the top 10 of all blogs globally and is often ranked #1 in the categories of Science, Education, and Health & Fitness.
+        Airnesy – Discover what’s trending now. From viral sensations to breaking news,
+         join millions of viewers and stay in tune with the moments that matter most.
+          Explore the hottest trends with all the live commentary.
         </p>
-        <div className={styles.icons}>
-        <Link href="https://www.tiktok.com/@hubermanlabblog">
-
-          <Image src="/tiktok.png" alt="" width={45} height={28} /> </Link>
     
-        </div>
       </div>
       <div className={styles.links}>
         <div className={styles.list}>
-          <span className={styles.listTitle}>Tags</span>
-          <Link href="/blog?cat=sleep">sleep</Link>
-          <Link href="/blog?cat=fertility">fertility</Link>
-          <Link href="/blog?cat=hormones">hormones</Link>
-          <Link href="/blog?cat=memory">memory</Link>
+          <span className={styles.listTitle}>Top </span>
+          <Link href="/blog?cat=politics">politics</Link>
+          <Link href="/blog?cat=news">news</Link>
+          <Link href="/blog?cat=technology">technology</Link>
+          <Link href="/blog?cat=business">business</Link>
         </div>
         <div className={styles.list}>
           <span className={styles.listTitle}>Tags</span>
-          <Link href="/blog?cat=well-being">well-being</Link>
-          <Link href="/blog?cat=nutrition">nutrition</Link>
-          <Link href="/blog?cat=fitness">fitness</Link>
-          <Link href="/blog?cat=mental">mental</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Privacy Policy</Link>
+          <Link href="/">User Agreement</Link>
         </div>
         <div className={styles.list}>
           <span className={styles.listTitle}>Social</span>
-          <Link href="https://www.tiktok.com/@hubermanlabblog">Tiktok </Link>
+          <Link href="https://www.tiktok.com/@Airnesy">Tiktok </Link>
+          <Link href="https://www.youtube.com/@Airnesy">Youtube </Link>
+
         </div>
       </div>
     </div>

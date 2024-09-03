01a539568a60f8d6d02e81c4d2f40cd501baef7c
psnapyeditor <info.psnapy@gmail.com>
2024-09-03 17:52:22 +0300

    main first commet

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,6 @@ export default function RootLayout({ children }) {
               <div className="container">
                 <div className="wrapper">
                   {children}
-                  <Controls />
                 </div>
               </div>
             </ThemeProvider>

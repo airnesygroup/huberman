5aa9dbad45ec930d4b944a1dad787b6f205f5362
psnapyeditor <info.psnapy@gmail.com>
2024-09-03 23:12:19 +0300

    main first commet

--- src/components/cardList2/CardList2.jsx
+++ src/components/cardList2/CardList2.jsx
@@ -29,7 +29,7 @@ const CardList2 = async ({ page, cat }) => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>   popular</h1>
+      <h1 className={styles.title}>   Popular-Top Trending</h1>
       <div className={styles.posts}>
         {posts?.map((item) => (
           <Card item={item} key={item._id} />

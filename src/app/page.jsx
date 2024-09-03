78d20c804a03c7ab354dffd2024680c1d01c2744
psnapyeditor <info.psnapy@gmail.com>
2024-09-03 17:10:44 +0300

    main first commet

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { useState, useEffect } from "react";
 import Link from "next/link";
 import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
@@ -9,41 +6,13 @@ import CardList from "@/components/cardList/CardList";
 import SidebarCategoryList from "@/components/SidebarcategoryList/SidebarCategoryList";
 import Menu from "@/components/Menu/Menu";
 import CarouselList from "@/components/carouselList/CarouselList";
+import Controls from "@/components/Controls";
 import Trending from "@/components/Trending";
 import AddIcon from "@/components/Addicon";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
-
 export default function Home({ searchParams }) {
-  const page = parseInt(searchParams?.page || "1");
-
-  const [isNavbarVisible, setIsNavbarVisible] = useState(true);
-  const [isTrendingVisible, setIsTrendingVisible] = useState(false);
-  const [lastScrollY, setLastScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-
-      if (window.innerWidth < 720) {
-        if (currentScrollY > lastScrollY) {
-          setIsNavbarVisible(false);
-          setIsTrendingVisible(true);
-        } else {
-          setIsNavbarVisible(true);
-          setIsTrendingVisible(false);
-        }
-      }
-
-      setLastScrollY(currentScrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [lastScrollY]);
+  const page = parseInt(searchParams.page) || 1;
 
   return (
     <div className={styles.container}>
@@ -55,7 +24,7 @@ export default function Home({ searchParams }) {
         <Trending />
       </div>
 
-      {isNavbarVisible && <Navbar />}
+     <Navbar />
       <AddIcon />
 
       <div className={styles.menu}>
